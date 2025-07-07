a3c51b8c15b1c6845a76631e45bcb5003052677e
KyoDev <Sugaruchan2434@gmail.com>
2025-07-08 00:06:14 +0900

    Update main.py
    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+def show_log_preview(filepath):
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            for i, line in enumerate(file):
+                print(line.strip())
+                if i >= 9:
+                    break
+    except FileNotFoundError:
+        print("ログファイルが見つかりませんでした。")
+
+if __name__ == "__main__":
+    show_log_preview("data/sample.log")
